45e21c36ed43049156ce9d32069e5a32e345dbab
goto-nao <goto@makersmark.co.jp>
2021-11-07 21:25:00 +0900

    apply grig to input component

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Avatar, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
     root:{
-        gridRow:2
+        gridRow:2,
+        margin: '26px'
     }
 })
 const MessageInputField =()=>{
     const classes = useStyles();
     return(
         <div className={classes.root}>
-            MessageInputField
+            <Grid container>
+                <Grid xs={1}>
+                    <Avatar />
+                </Grid>
+                <Grid xs={10}>input</Grid>
+                <Grid xs={1}>button</Grid>
+            </Grid>
         </div>
     )
 }
