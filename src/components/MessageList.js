6937f5d9d895d21ea6572317365ae7949514bc1f
goto-nao <goto@makersmark.co.jp>
2021-11-07 21:08:21 +0900

    apply grid row

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+    root:{
+        gridRow:1,
+    }
+})
 const MessageList =()=>{
+    const classes = useStyles();
     return(
         <div>
             MessageList
