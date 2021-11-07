8be7688c1e2ff03c696ff4c57bfade53c7ecba44
goto-nao <goto@makersmark.co.jp>
2021-11-07 22:05:23 +0900

    apply gravatar.js

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Avatar, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+import { gravatarPath} from '../gravatar';
+
 const useStyles = makeStyles({
     root:{
         gridRow:2,
         margin: '26px'
     }
 })
-const MessageInputField =()=>{
+const MessageInputField =({name})=>{
     const classes = useStyles();
+    const avatarPath = gravatarPath('goto@makersmark.co.jp');
     return(
         <div className={classes.root}>
             <Grid container>
                 <Grid xs={1}>
-                    <Avatar />
+                    <Avatar src={avatarPath}/>
                 </Grid>
                 <Grid xs={10}>input</Grid>
                 <Grid xs={1}>button</Grid>
